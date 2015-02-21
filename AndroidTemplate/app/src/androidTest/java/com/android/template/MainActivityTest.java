6962bd5e28fb3347e4a412903527a9696229c9b5
Isac Sacchi e Souza <isacssouza@gmail.com>
2015-02-21 13:09:11 -0200

    Use robotium for instrumentation tests

---
+++ AndroidTemplate/app/src/androidTest/java/com/android/template/MainActivityTest.java
@@ -0,0 +1,41 @@
+package com.android.template;
+
+import android.support.v7.widget.Toolbar;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.android.template.androidtemplate.R;
+import com.robotium.solo.Solo;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private Solo solo;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    public void tearDown() {
+        solo.finishOpenedActivities();
+    }
+
+    public void testClickOnAction() {
+        solo.scrollToSide(Solo.LEFT);
+
+        solo.clickOnView(solo.getView(R.id.action_example));
+
+        solo.waitForText("Example action.");
+    }
+
+    public void testHasToolbar() {
+        MainActivity mainActivity = getActivity();
+
+        Toolbar toolbar = (Toolbar) solo.getView(R.id.toolbar);
+
+        assertNotNull(toolbar);
+    }
+}
