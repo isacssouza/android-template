aa3ad2019785c305b9b908770e52fcfe9fb29632
Isac Sacchi e Souza <isacssouza@gmail.com>
2015-04-23 21:58:19 -0300

    Update appcompat libraries

--- AndroidTemplate/app/src/main/java/com/android/template/ui/MainActivity.java
+++ AndroidTemplate/app/src/main/java/com/android/template/ui/MainActivity.java
@@ -10,7 +10,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -38,7 +38,7 @@ import dagger.ObjectGraph;
 import rx.functions.Action1;
 
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
     private static final String TAG = MainActivity.class.getSimpleName();
 
